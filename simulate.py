babcce23000033f5d7432d8c2d88aac5222a2f05
Jessica Rosseau <jessica@Jessicas-MacBook-Pro.local>
2024-02-12 15:15:44 -0500

    still working on step 22

--- simulate.py
+++ simulate.py
@@ -1,6 +1,7 @@
 import pybullet as p
 import time
 import pybullet_data
+import numpy
 
 from pyrosim import pyrosim
 
@@ -11,11 +12,18 @@ planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 
 pyrosim.Prepare_To_Simulate(robotId)
+backLegSensorValues = numpy.zeros(10000)
+print(backLegSensorValues)
+exit()
+
 #p.loadSDF("world.sdf")
 for i in range(1000):
 	p.stepSimulation()
-	backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+	#backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+	backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_for_Link("BackLegSensor")
 	time.sleep(1/60)
-	print(backLegTouch)
+	#print(backLegTouch)
 
 p.disconnect()
+print(backLegSensorValues)
+#exit()
