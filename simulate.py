3d5e872d4add01efb94c7e6b67cda76492c58dbc
Jessica Rosseau <jessica@Jessicas-MacBook-Pro.local>
2024-02-15 20:21:29 -0500

    up to step 14

--- simulate.py
+++ simulate.py
@@ -64,6 +64,12 @@ for i in range(1000):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+    pyrosim.Set_Motor_For_Joint(
+        bodyIndex= robotId,
+        jointName="Torso_BackLeg",
+        controlMode=p.POSITION_CONTROL,
+        targetPosition=0.0,
+        maxForce=500)
     time.sleep(1/60)
 
 p.disconnect()
