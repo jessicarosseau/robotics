f9726aedbf140b163777123216a344e1532dfc74
Jessica Rosseau <jessica@ip0af5c984.int.uvm.edu>
2024-02-23 12:39:38 -0500

    updating branch

--- simulate.py
+++ simulate.py
@@ -42,13 +42,14 @@
 # numpy.save(backLegFile, backLegSensorValues)
 # numpy.save(frontLegFile, frontLegSensorValues)
 import math
-
 import pybullet as p
 import time
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
 import numpy
 import os
+import math
+import random
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -59,22 +60,53 @@ robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate(robotId)
 
+num_steps = 1000
+t = numpy.linspace(0, 2 * numpy.pi, num_steps)  # Create a time array
+
+# frontLeg
+front_amplitude = math.pi / 4
+front_frequency = 10
+front_phaseOffset = 0
+front_targetAngles = front_amplitude * numpy.sin(front_frequency * t + front_phaseOffset)
+
+# backLeg
+back_amplitude = math.pi / 4
+back_frequency = 10
+back_phaseOffset = 1
+back_targetAngles = back_amplitude * numpy.sin(back_frequency * t + back_phaseOffset)
+
+
+# numpy.save('data/front_targetAngles.npy', front_targetAngles)
+# numpy.save('data/back_targetAngles.npy', back_targetAngles)
+# exit()
+
 backLegSensorValues = numpy.zeros(1000)
 frontLegSensorValues = numpy.zeros(1000)
-for i in range(1000):
+for i in range(num_steps):
     p.stepSimulation()
+
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
-    frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
     pyrosim.Set_Motor_For_Joint(
         bodyIndex=robotId,
         jointName=b'Torso_BackLeg',
         controlMode=p.POSITION_CONTROL,
-        targetPosition= +math.pi/4.0,
-        maxForce=500)
-    time.sleep(1/60)
+        targetPosition=back_targetAngles[i],
+        maxForce=20
+    )
+
+    frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+    pyrosim.Set_Motor_For_Joint(
+        bodyIndex=robotId,
+        jointName=b'BackLeg_FrontLeg',
+        controlMode=p.POSITION_CONTROL,
+        targetPosition=-front_targetAngles[i],
+        maxForce=20
+    )
+    time.sleep(1/240)
 
 p.disconnect()
 print(backLegSensorValues)
+print('--------------------------- \n')
 print(frontLegSensorValues)
 subdirectory = 'data'
 
