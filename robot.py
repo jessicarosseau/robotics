43c8ae1e8f7d96c2703615acbd008995aaef62b8
Jessica Rosseau <jessica@ip0af5c984.int.uvm.edu>
2024-02-23 14:08:56 -0500

    up to step 27

--- robot.py
+++ robot.py
@@ -1,6 +1,9 @@
+from sensor import SENSOR
+from motor import MOTOR
+
 class ROBOT:
 
     def __init__(self):
-
-        pass
+        self.sensors = SENSOR()
+        self.motors = MOTOR()
 
--- simulation.py
+++ simulation.py
@@ -1,9 +1,29 @@
 from robot import ROBOT
 from world import WORLD
 
+import math
+import pybullet as p
+import time
+import pybullet_data
+import pyrosim.pyrosim as pyrosim
+import numpy
+import os
+import math
+import constants as  c
+import random
+
 
 class SIMULATION:
 
     def __init__(self):
         self.world = WORLD()
         self.robot = ROBOT()
+
+        physicsClient = p.connect(p.GUI)
+        p.setAdditionalSearchPath(pybullet_data.getDataPath())
+        p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+        p.setGravity(0, 0, -9.8)
+        self.planeId = p.loadURDF("plane.urdf")
+        self.robotId = p.loadURDF("body.urdf")
+        p.loadSDF("world.sdf")
+        pyrosim.Prepare_To_Simulate(self.robotId)
